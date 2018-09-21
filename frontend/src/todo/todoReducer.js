d3605a9111c0f006f769ae1fac441dc407811f94
RicardoG <Guilherme.RicardoCosta@BR.unisys.com>
2018-09-21 16:37:01 -0300

    Finalização frontend

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,26 +1,16 @@
 const INITIAL_STATE = {
-  description: 'Ler Livro',
-  list: [{
-    _id: 1,
-    description: 'Pagar fatura do cartão',
-    done: true
-  }, {
-    _id: 2,
-    description: 'Reunião com a equipe às 10:00',
-    done: false
-  }, {
-    _id: 3,
-    description: 'Consulta médica na terça depois do almoço',
-    done: false
-  }]
-}
+  description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'DESCRIPTION_CHANGED':
       return { ...state, description: action.payload }
     case 'TODO_SEARCHED':
-      return { ...state, list: action.payload.data }
+      return { ...state, list: action.payload }
+    case 'TODO_CLEAR':
+      return { ...state, description: '' }
+    case 'TODO_REMOVED':
+      return { ...state, description: '' }
   default:
     return state
   }
